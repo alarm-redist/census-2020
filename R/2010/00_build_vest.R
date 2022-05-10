a83219c9b750099e4103a637f0d6a7e0a4d41e13
Christopher Kenny <ctk44@cornell.edu>
2022-05-10 14:51:47 -0400

    block data

--- R/2010/00_build_vest.R
+++ R/2010/00_build_vest.R
@@ -27,46 +27,55 @@ years <- c(2016, 2018, 2020)
 
 # run
 for (year in years) {
-  states <- vest_states(year)
-  states <- states[states != 'ca']
-  for (state in states) {
-    fs::dir_create(here('vest-2010', state))
+    states <- vest_states(year)
+    states <- states[states != 'ca']
+    for (state in states) {
+        fs::dir_create(here('vest-2010', state))
 
-    if (!fs::file_exists(here(glue::glue('vest-2010/{state}/{state}_{year}_2010_block_data.csv'))) &
-      !fs::file_exists(here(glue::glue('vest-2010/{state}/{state}_{year}_2010_vtd_data.csv')))) {
+        if (!fs::file_exists(here(glue::glue('vest-2010/{state}/{state}_{year}_2010_block_data.csv'))) &
+            !fs::file_exists(here(glue::glue('vest-2010/{state}/{state}_{year}_2010_vtd_data.csv')))) {
 
 
-      # Step 1: Get 2010 Census blocks ----
-      block <- build_dec('block', state = state, groups = 'all', year = 2010) %>%
-        select(-NAME) %>%
-        st_transform(3875)
-      vest <- get_vest(state = state, year = year, clean_names = FALSE) %>%
-        st_transform(st_crs(block))
+            # Step 1: Get 2010 Census blocks ----
+            block <- build_dec('block', state = state, groups = 'all', year = 2010) %>%
+                select(-NAME) %>%
+                st_transform(3875)
+            vest <- get_vest(state = state, year = year, clean_names = FALSE) %>%
+                st_transform(st_crs(block))
 
-      # Step 2: Match Blocks to VEST data ----
-      match_list <- geo_match(from = block, to = vest, method = 'centroid')
+            # Step 2: Match Blocks to VEST data ----
+            match_list <- geo_match(from = block, to = vest, method = 'centroid')
 
-      # Step 3: Estimate election data down to 2010 blocks ----
-      elec_at_2010 <- block %>%
-          select(starts_with('GEOID'), starts_with('pop'), starts_with('vap'))
-      elections <- names(vest)[str_detect(names(vest), str_c('G', year - 2000)) |
-        str_detect(names(vest), str_c('R', year - 2000))]
-      for (election in elections) {
-        elec_at_2010 <- elec_at_2010 %>%
-          mutate(!!election := estimate_down(
-            value = vest[[election]], wts = block[['vap']],
-            group = match_list
-          ))
-      }
+            # Step 3: Estimate election data down to 2010 blocks ----
+            elec_at_2010 <- block %>%
+                as_tibble(-any_of('geometry')) %>%
+                select(starts_with('GEOID'))
+            elections <- names(vest)[str_detect(names(vest), str_c('G', year - 2000)) |
+                                         str_detect(names(vest), str_c('R', year - 2000))]
+            for (election in elections) {
+                elec_at_2010 <- elec_at_2010 %>%
+                    mutate(!!election := estimate_down(
+                        value = vest[[election]], wts = block[['vap']],
+                        group = match_list
+                    ))
+            }
 
-      # Step 4: Save 2010 block-level outputs ----
-      elec_at_2010 <- elec_at_2010 %>%
-          mutate(across(where(is.numeric), round, digits=1))
-        write_csv(elec_at_2010, file = here(glue::glue('vest-2010/{tolower(state)}/{tolower(state)}_{year}_2010_block_data.csv')))
-      }
+            # Step 4: Save 2010 block-level outputs ----
+            elec_at_2010 <- elec_at_2010 %>%
+                mutate(across(where(is.numeric), round, digits=1))
+            write_csv(elec_at_2010, file = here(glue::glue('vest-2010/{tolower(state)}/{tolower(state)}_{year}_2010_block_data.csv')))
+        }
 
-      log_time(here("vest-2010/log_time.txt"), state, year)
+        log_time(here("vest-2010/log_time.txt"), state, year)
     }
-  }
+}
 
 withr::deferred_clear()
+
+files <- fs::dir_ls('vest-2010', recurse = TRUE, glob = '*.csv')
+
+lapply(files, function(f) {
+    read_csv(f) %>%
+        select(-starts_with('pop'), -starts_with('vap'), -any_of('geometry')) %>%
+        write_csv(file = f)
+})
---
+++ R/2010/01_compile.R
@@ -0,0 +1,112 @@
+# libs ----
+library(tidyverse)
+library(PL94171)
+library(here)
+library(censable)
+library(dataverse)
+
+# helpers ----
+log_time <- function(path, state) {
+  if (!fs::file_exists(path)) {
+    fs::file_create(path)
+  }
+  lines <- readLines(con = path)
+  lines <- c(lines, paste0(Sys.time(), ',', state))
+  writeLines(text = lines, con = path)
+}
+
+download_2010_baf <- function(state) {
+    url <- str_glue('https://www2.census.gov/geo/docs/maps-data/data/baf/BlockAssign_ST{match_fips(state)}_{toupper(match_abb(state))}.zip')
+    tf <- fs::file_temp(ext = 'zip')
+    httr::GET(url = url, httr::write_disk(tf))
+    zip::unzip(zipfile = tf, exdir = fs::path_dir(tf))
+    path_vtd <- fs::path(fs::path_dir(tf), str_glue('BlockAssign_ST{match_fips(state)}_{toupper(match_abb(state))}_VTD.txt'))
+    if (fs::file_exists(path_vtd)) {
+        read_csv(file = path_vtd)
+    } else {
+        NULL
+    }
+}
+
+# globals ----
+states <- censable::stata %>%
+  filter(!is.na(region)) %>%
+  pull(abb)
+
+vest_path <- here('vest-2010')
+joined_path <- here('census-vest-2010')
+
+for (s in states) {
+  vest_files <- Sys.glob(str_glue('{vest_path}/{str_to_lower(s)}/*.csv'))
+
+  # download and subset pl ----
+  pl <- pl_read(pl_url(s, 2010)) %>%
+    pl_select_standard(clean_names = TRUE)
+  type <- 'vtd'
+  state_d <- pl_subset(pl, '700')
+
+  # check that the state has vtds ----
+  if (any(str_detect(vest_files, '_block\\.csv')) || nrow(state_d) == 0) { # no VTDs
+    type <- 'block'
+    state_d <- pl_subset(pl, '750') %>%
+      select(-vtd)
+  }
+
+  fips_d <- censable::fips_2020 %>%
+    left_join(select(censable::stata, fips, abb), by = c('state' = 'fips')) %>%
+    select(state = abb, county_code = county, county = name)
+
+  state_d <- state_d %>%
+    select(-.data$row_id, -.data$summary_level) %>%
+    rename(county_code = county, GEOID20 = GEOID) %>%
+    left_join(tigris::fips_codes, by = c('state', 'county_code')) %>%
+    select(-county_code, -state_code, -state_name) %>%
+    relocate(county, .after = state)
+
+  if (length(vest_files) > 0) {
+    spec <- cols(GEOID20 = 'c')
+    if (type != 'vtd') {
+      spec <- cols(GEOID = 'c')
+    }
+
+    # read files ----
+    vest_d <- map(vest_files, read_csv, show_col_types = FALSE, col_types = spec)
+
+    # select elections and dem & rep ----
+    vest_d <- vest_d %>%
+      map(~ rename_with(., ~'GEOID20', ends_with('GEOID'))) %>%
+      map(~ select(., GEOID20, starts_with('G'), starts_with('R'))) %>% # gen/runoff election
+      map(geomander::clean_vest) %>%
+      map(~ select(., GEOID20, starts_with(c('pre', 'uss', 'gov', 'atg', 'sos')) &
+        contains(c('_rep_', '_dem_')))) %>%
+      reduce(left_join, by = 'GEOID20')
+
+    # Average ----
+    vest_d <- vest_d %>%
+      mutate(
+        arv_16 = rowMeans(select(., contains('_16_rep_')), na.rm = TRUE),
+        adv_16 = rowMeans(select(., contains('_16_dem_')), na.rm = TRUE),
+        arv_18 = rowMeans(select(., contains('_18_rep_')), na.rm = TRUE),
+        adv_18 = rowMeans(select(., contains('_18_dem_')), na.rm = TRUE),
+        arv_20 = rowMeans(select(., contains('_20_rep_')), na.rm = TRUE),
+        adv_20 = rowMeans(select(., contains('_20_dem_')), na.rm = TRUE),
+        nrv = rowMeans(select(., contains('_rep_')), na.rm = TRUE),
+        ndv = rowMeans(select(., contains('_dem_')), na.rm = TRUE),
+        ndv = round(ndv, 1),
+        nrv = round(nrv, 1),
+        across(c(starts_with("adv_"), starts_with("arv_")),
+               round, digits=1)
+      )
+
+    # Round ----
+    vest_d <- vest_d %>%
+      mutate(across(where(is.numeric), .fns = \(x) round(x, 1)))
+
+    state_d <- state_d %>% left_join(vest_d, by = 'GEOID20')
+  }
+
+  write_csv(state_d, str_glue('{joined_path}/{str_to_lower(s)}_2010_{type}.csv'))
+  log_time(here('census-vest-2010/log_time.txt'), s)
+
+  fs::file_delete(fs::dir_ls(fs::path(tempdir(),'PL-unzip')))
+}
